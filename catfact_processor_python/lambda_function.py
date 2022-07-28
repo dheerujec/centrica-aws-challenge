2d14303d23b3708f15d50e90d338ca47f11e5503
Dharmendra Patel <Dharmendra.Patel2@bp.com>
2022-07-28 01:28:04 +0100

    Update to the code to cater interview scenario for not returning same fact in two consicutive calls of catfact api

--- catfact_processor_python/lambda_function.py
+++ catfact_processor_python/lambda_function.py
@@ -1,8 +1,18 @@
 import json
 import random
 
-def lambda_handler(event, context):
+import boto3
 
+
+def lambda_handler(event, context):
+    s3_client = boto3.client('s3')
+    
+    try:
+        resp = s3_client.get_object(Bucket="centrica-aws-challenge-01", Key="catfact_previous_resp.csv")
+        catfact_prev_resp = resp['Body'].read().decode()
+        print(f"Previous Response: {catfact_prev_resp}")
+    except Exception as e:
+        print(e)
     catfact_list = [
         "The longest living cat on record according to the Guinness Book belongs to the late Creme Puff of Austin, Texas who lived to the ripe old age of 38 years and 3 days!",
         "Cats have \"nine lives\" thanks to a flexible spine and powerful leg and back muscles",
@@ -10,8 +20,13 @@ def lambda_handler(event, context):
         "Florence Nightingale owned more than 60 cats in her lifetime.",
         "A cat sees about 6 times better than a human at night, and needs 1/6 the amount of of light that a human does - it has a layer of extra reflecting cells which absorb light."
     ]
-
-    fact = random.choice(catfact_list)
+    fact = catfact_prev_resp
+    counter = 0
+    while fact == catfact_prev_resp:
+        fact = random.choice(catfact_list)
+        counter += 1
+    print(f"Counter: {counter}")
+    resp = s3_client.put_object(Bucket="centrica-aws-challenge-01", Key="catfact_previous_resp.csv", Body=fact)
     length = len(fact)
     return {
             "fact": fact,
